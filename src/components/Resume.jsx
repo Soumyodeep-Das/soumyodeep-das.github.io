b6dbb7cc327b41f9fb4ffaa93e055223de903c97
Soumyodeep-Das <linux.programmer.06@gmail.com>
2024-10-17 01:03:41 +0530

    Improved Resume section

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,19 +2,19 @@ import React from "react";
 
 const Resume = () => {
   return (
-    <div className="flex justify-center items-center min-h-80 bg-gray-100 px-4">
+    <div className="flex justify-center items-center min-h-80 bg-gray-100 px-4 py-4">
       <div className="resume-card p-6 sm:p-8 max-w-sm w-full bg-white rounded-xl shadow-md space-y-4">
         <h2 className="text-lg sm:text-xl font-medium text-black text-center">
           Download My Resume
         </h2>
 
         <img
-          src="/path/to/your/resume-thumbnail.jpg"
+          src="src/assets/resume-preview.png"
           alt="Resume Thumbnail"
-          className="w-full h-auto rounded-md object-cover"
+          className="w-auto h-2/5 rounded-md object-cover"
         />
 
-        <a href="/path/to/your/resume.pdf" download>
+        <a href="https://docs.google.com/document/d/1U8dSM1mE37_yZbwRV-zzhmEx9bDvJjvpgE8e7klR2oQ/edit?usp=drive_link" download target="_blank">
           <button className="w-full mt-4 px-4 py-2 bg-blue-500 text-white text-sm font-medium rounded-md hover:bg-blue-700 transition duration-300">
             Download
           </button>
