91047e3527df1ccdce6e356431fa5a3ef685e9ad
Soumyodeep-Das <linux.programmer.06@gmail.com>
2024-10-17 20:52:33 +0530

    updated resume file link

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -15,7 +15,7 @@ const Resume = () => {
           className="w-auto h-2/5 rounded-md object-cover"
         />
 
-        <a href="https://docs.google.com/document/d/1U8dSM1mE37_yZbwRV-zzhmEx9bDvJjvpgE8e7klR2oQ/edit?usp=drive_link" download target="_blank">
+        <a href="https://drive.google.com/file/d/1HRNlaHyVioJ1uUmCvIDZD0Q3RqFb1269/view?usp=sharing" download target="_blank">
           <button className="w-full mt-4 px-4 py-2 bg-blue-500 text-white text-sm font-medium rounded-md hover:bg-blue-700 transition duration-300">
             Download
           </button>
